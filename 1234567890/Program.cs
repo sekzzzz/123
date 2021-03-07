327a800199d7f9edd13438bf4b27d918c289c433
unknown <diman124000544@gmail.com>
2021-03-07 17:18:19 +0300

    change

---
+++ 1234567890/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1234567890
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
